77c76878bc80a8d1233ab33025c93d975c658c54
mrjuniors <sergio.rebelo@mysp.pt>
2022-06-24 17:49:20 +0100

    Modulo 10 - Aula - 02

--- app/Repositories/Api/CourseRepository.php
+++ app/Repositories/Api/CourseRepository.php
@@ -20,7 +20,7 @@ class CourseRepository
 
     public function getAllCourses()
     {
-        return $this->entity->with('modules.lessons')->get();
+        return $this->entity->with('modules.lessons.views')->get();
     }
 
     public function getCourse(string $identify)
